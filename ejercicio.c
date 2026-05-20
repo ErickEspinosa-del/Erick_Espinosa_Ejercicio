b0fad94f66c617e74b90435f7d8e15ff8ee0d101
Erick Espinosa <erick.espinosa.morejon@udla.edu.ec>
2026-05-20 14:32:20 -0500

    Semilla de codigo para el ejercicio_miercoles

--- ejercicio.c
+++ ejercicio.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
+    float notas[5] = {6.5, 7.1, 8, 6, 5};
+    for (int i = 0; i < 5; i++)
+
+    {
+      printf("notas[%d]%.1f\n",i,notas[i]);
+    }
+    
     return 0;
 }
